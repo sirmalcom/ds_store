104508e194ebeee04c48197d90a7a249770e1985
Alexandersan <i1921258@continental.edu.pe>
2022-11-18 14:49:03 -0500

    Frontend, diseño home y empezando redux

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import Header from './component/Home/Header'
+import Home from './component/Home/Home.jsx'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
